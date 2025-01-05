46828239a99e394f187450eb02c37e9fdd2a6d30
Vladut Gidea <vladutgidea.ro@gmail.com>
2025-01-05 20:15:00 +0200

    nice

--- ptGit/PersonalProj7/incercarea1/Forms/ConnectionStringForm.Designer.cs
+++ ptGit/PersonalProj7/incercarea1/Forms/ConnectionStringForm.Designer.cs
@@ -49,6 +49,7 @@
             this.connectionStringTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.connectionStringTB.Location = new System.Drawing.Point(219, 79);
             this.connectionStringTB.Name = "connectionStringTB";
+            this.connectionStringTB.PasswordChar = '*';
             this.connectionStringTB.Size = new System.Drawing.Size(355, 24);
             this.connectionStringTB.TabIndex = 14;
             // 
@@ -83,7 +84,7 @@
             this.Controls.Add(this.saveBTN);
             this.Controls.Add(this.connectionStringTB);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ConnectionStringForm";
